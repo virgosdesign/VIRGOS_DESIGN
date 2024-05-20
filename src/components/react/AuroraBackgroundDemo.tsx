614ccf5d410c0c7612f62943b6c7fc51a6fb01cc
Keith Robinson <keith.robinson@dtn.com>
2024-05-19 18:43:17 -0700

    Add full-body background in fixed position, for depth effect.

--- src/components/react/AuroraBackgroundDemo.tsx
+++ src/components/react/AuroraBackgroundDemo.tsx
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion';
 
 export function AuroraBackgroundDemo() {
   return (
-    <AuroraBackground>
+    // <AuroraBackground>
+    <div className="relative flex flex-col  h-lvh items-center justify-center  text-slate-950 transition-bg">
       <motion.div
         initial={{ opacity: 0.0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -15,9 +16,7 @@ export function AuroraBackgroundDemo() {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Welcome to Virgos Design.
-        </div>
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">Welcome to Virgos Design.</div>
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
           somewhere next to heaven.
         </div>
@@ -25,6 +24,7 @@ export function AuroraBackgroundDemo() {
           Get Started
         </button>
       </motion.div>
-    </AuroraBackground>
+    </div>
+    // </AuroraBackground>
   );
 }
