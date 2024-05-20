5c760a68549d2ff6ef7c679f17dbccce49f010ec
Keith Robinson <keith.robinson@dtn.com>
2024-05-19 17:29:15 -0700

    Update variable

--- src/components/react/aceternityui/FloatingNav.tsx
+++ src/components/react/aceternityui/FloatingNav.tsx
@@ -19,57 +19,46 @@ export const FloatingNav = ({
   const [visible, setVisible] = useState(true);
   const [position, setPosition] = useState('absolute');
   const [placement, setPlacement] = useState('bottom-20');
+  const [shadow, setShadow] = useState('shadow-none');
+  const [blur, setBlur] = useState('backdrop-blur-none');
 
   useMotionValueEvent(scrollYProgress, 'change', (current) => {
     const page = document.documentElement;
     const nav = document.querySelector('#floating-nav') as HTMLElement;
-    const X = page.clientHeight - page.scrollTop - nav.offsetHeight - 200;
+    const positionDelta = 200; // For large screens, nav placment differential is bottom-20/top-20, a 200px difference
+    const X = page.clientHeight - page.scrollTop - nav.offsetHeight - positionDelta;
     const Y = page.clientHeight - page.scrollTop;
-    console.log('X', X);
-    console.log('Y', Y);
+
     // Check if current is not undefined and is a number
     if (typeof current === 'number') {
       let direction = current! - scrollYProgress.getPrevious()!;
       if (direction > 0) {
         console.log('Scrolling down');
-        console.log('current', current);
-        console.log('scrollYProgress.get()', scrollYProgress.get());
-        console.log('X', X)// - nav.offsetHeight)
-        console.log('NAV TOP POSITION:', nav.offsetTop)
+        // Once nav reaches top of screen, it will become fixed there.
         if (X < 0){
           console.log('Set fixed');
           setPosition('fixed');
           setPlacement('top-20');
-          //setVisible(false);
+          setShadow(
+            'shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'
+          );
+          setBlur('backdrop-blur-lg');
         }
-        if (Y < 0){
-          console.log('Set invisible <<<<<  A');
+        // Once halfway down second page, nav will hide
+        if (Y < -(page.clientHeight/2)) {
           setVisible(false);
         }
-      
-        
       } else {
         console.log('Scrolling up');
         setVisible(true);
         if (X >= 0){
-          console.log('Set absolute');
+          console.log('set sticky')
           setPosition('absolute');
           setPlacement('bottom-20');
+          setShadow('shadow-none');
+          setBlur('backdrop-blur-none');
         }
       }
-
-      // if (scrollYProgress.get() < 0.05) {
-      //   console.log('Set invisible <<<<<  A');
-      //   //setVisible(false);
-      // } else {
-      //   if (direction < 0) {
-      //     console.log('Set visible >>>>>>>>>>>>>>');
-      //     //setVisible(true);
-      //   } else {
-      //     console.log('Set invisible <<<<<  B');
-      //     //setVisible(false);
-      //   }
-      // }
     }
   });
 
@@ -89,7 +78,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={classNames(
-          `flex max-w-fit ${position} ${placement} z-50 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4`,
+          `flex max-w-fit ${position} ${placement} ${shadow} ${blur} z-50 inset-x-0 mx-auto bg-white/20 dark:bg-black/20 border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4`,
           className
         )}
       >
