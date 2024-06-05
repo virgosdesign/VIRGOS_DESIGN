f8816ab516efb911b8f1cf4784c9d3d180f14a7e
Keith Robinson <keith.robinson@dtn.com>
2024-06-04 17:26:03 -0700

    Work on navigation to contract, instead of slide away.

--- src/components/react/aceternityui/FloatingNav.tsx
+++ src/components/react/aceternityui/FloatingNav.tsx
@@ -1,29 +1,42 @@
 import { AnimatePresence, motion, useMotionValueEvent, useScroll } from 'framer-motion';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ToggleTheme } from '../ToggleTheme';
 import { classNames } from '@utils/utils';
+
+type NavItem = {
+  name: string;
+  link: string;
+  icon?: JSX.Element;
+};
 
 export const FloatingNav = ({
   navItems,
   className,
 }: {
-  navItems: {
-    name: string;
-    link: string;
-    icon?: JSX.Element;
-  }[];
+  navItems: NavItem[];
   className?: string;
 }) => {
   const { scrollYProgress } = useScroll();
 
+  const [leftNavigation, setLeftNavigation] = useState<NavItem[]>([]);
+  const [rightNavigation, setRightNavigation] = useState<NavItem[]>([]);
   const [visible, setVisible] = useState(true);
+  const [expanded, setExpanded] = useState(true);
   const [position, setPosition] = useState('absolute');
   const [placement, setPlacement] = useState('bottom-20');
   const [shadow, setShadow] = useState('shadow-none');
   const [blur, setBlur] = useState('backdrop-blur-none');
   const [background, setBackground] = useState('bg-none');
   const [border, setBorder] = useState('border border-transparent dark:border-transparent');
+
+  useEffect(() => {
+    const half = Math.ceil(navItems.length / 2);
+    const left = navItems.slice(0, half);
+    const right = navItems.slice(half, navItems.length);
+    setLeftNavigation(left);
+    setRightNavigation(right);
+  }, []);
 
   useMotionValueEvent(scrollYProgress, 'change', (current) => {
     const page = document.documentElement;
@@ -35,19 +48,18 @@ export const FloatingNav = ({
     // Check if current is not undefined and is a number
     if (typeof current === 'number') {
     console.log("Current:", current)
-      if (current === 0) {
+      if (current === 0) { // Reset the styles to initial (invisible borders, no shadow, no blur, no background)
         setShadow('shadow-none');
         setBlur('backdrop-blur-none');
         setBorder('border-transparent dark:border-transparent');
       }
       let direction = current! - scrollYProgress.getPrevious()!;
-      if (direction > 0) {
+      if (direction > 0) { // Style the navigation so that it is a visible overlay
         console.log('Scrolling down');
         setShadow(
           'shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'
         );
-        // Once nav reaches top of screen, it will become fixed there.
-        if (X < 0){
+        if (X < 0){ // Once nav reaches top of screen, fix it there
           console.log('Set fixed');
           setPosition('fixed');
           setPlacement('top-20');
@@ -57,13 +69,14 @@ export const FloatingNav = ({
           setBlur('backdrop-blur-lg');
           setBorder('dark:border-slate-600')
         }
-        // Once halfway down second page, nav will hide
-        if (Y < -(page.clientHeight/2)) {
-          setVisible(false);
+        if (Y < -(page.clientHeight/2)) { // Once nav reaches bottom of screen, apply cloaking transformation
+          setExpanded(false);
+          //setVisible(false);
         }
       } else {
         console.log('Scrolling up');
-        setVisible(true);
+        //setVisible(true);
+        setExpanded(true);
         if (X >= 0){
           console.log('set sticky')
           setPosition('absolute');
@@ -79,7 +92,7 @@ export const FloatingNav = ({
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        id='floating-nav'
+        id="floating-nav"
         initial={{
           opacity: 1,
           y: -100,
@@ -96,7 +109,30 @@ export const FloatingNav = ({
           className
         )}
       >
-        {navItems.map((navItem: any, idx: number) => (
+        {expanded && 
+        <AnimatePresence mode="wait">
+        {leftNavigation.map((navItem: any, idx: number) => (
+          <motion.a
+            key={`link=${idx}`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.7 }}
+            //exit={{ opacity: 1 }}
+            href={navItem.link}
+            className={classNames(
+              'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500'
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden sm:block text-sm">{navItem.name}</span>
+          </motion.a>
+        ))}
+        </AnimatePresence>
+        }
+        <ToggleTheme />
+        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"> */}
+        <span className="text-sm">Login</span>
+        {expanded && rightNavigation.map((navItem: any, idx: number) => (
           <a
             key={`link=${idx}`}
             href={navItem.link}
@@ -108,12 +144,27 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </a>
         ))}
-        <ToggleTheme />
-        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"> */}
-          <span className="text-sm">Login</span>
-          {/* <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" /> */}
+        {/* <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" /> */}
         {/* </button> */}
       </motion.div>
+      {/* <motion.div
+        id="hamburger"
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: visible ? 0 : 1,
+        }}
+        transition={{
+          duration: 0.7,
+        }}
+        className={classNames(
+          `flex max-w-fit border ${border} ${position} ${placement} ${shadow} ${blur} ${background} inset-x-0 mx-auto border rounded-full z-[5000] px-6 py-2 items-center justify-center space-x-4`,
+          className
+        )}
+      >
+        Logo
+      </motion.div> */}
     </AnimatePresence>
   );
 };
